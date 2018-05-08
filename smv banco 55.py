43c9d64d47323b1f9f6f732475ab7628093532f6
Martin Corvera Pose <ma.corv@gmail.com>
2018-05-08 00:59:46 -0300

    Modificaciones al SMV Banco
    Se modifico el archivo

--- smv banco 55.py
+++ smv banco 55.py
@@ -1,53 +1,85 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 28 09:08:08 2018
 
-@author: Fernando
-"""
-import pandas as pd
 import numpy as np
-from sklearn import svm
+
+import pandas as pd
+
+import seaborn.apionly as sns
+
 import matplotlib.pyplot as plt
 
-home=pd.read_excel('C://Users//Fernando//Desktop//homeb.xlsx')
+ 
 
-print(home)
+from sklearn.cross_validation import cross_val_score
 
-home.describe()
+from sklearn.model_selection import StratifiedKFold
 
-home.columns
+from sklearn.svm import SVR
 
-x=home[['pred','finde', 'feriado', 'posferiado', 'habil',
-       'primer_habil', 'ultimo_habil']]
-y=home['obs']
-print(x)
+ 
 
-clf = svm.SVR()
-clf.fit(x, y) 
-clf = svm.SVR(C=99999999.0,  epsilon=50000, gamma=30,  kernel='rbf' )
+from sklearn.model_selection import train_test_split
 
-clf.fit(x, y)
+home_banking = pd.read_excel(r'/Users/martin/Documents/GitHub/practica-dma/datasets/homeb.xlsx')
 
-ee=clf.predict(x)
+ 
+
+from sklearn import svm
+
+home_banking.columns
+
+X=home_banking[[ 'pred','finde', 'feriado','posferiado', 'habil', 'primer_habil', 'ultimo_habil']]
+
+y=home_banking['obs']
+
+ 
+
+ 
+
+clf=svm.SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=10000, gamma=1,kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+
+clf.fit(X, y)
+
+ee=clf.predict(X)
+
 
 #%%
+
+ 
+
+ 
+
+ 
+
+
+#Genero el tiempo
+
+ 
 
 t=[i for i in range(0,len(ee))]
 
  
-bg_color = 'black'
 
-fg_color = 'white'
+ 
 
-fig = plt.figure(1,figsize=(5,5), facecolor=bg_color, edgecolor=fg_color)
+ 
 
-plt.figure(1,figsize=(5,5), axisbg='red')
+ 
+
+ 
+
+bg_color = 'white'
+
+fg_color = 'black'
+
+fig = plt.figure(1,figsize=(5,5),facecolor=bg_color, edgecolor=fg_color)
+
+plt.figure(1,figsize=(5,5))
 
 axes = plt.axes((0.1, 0.1, 0.8, .8), axisbg=bg_color)
 
-axes.xaxis.set_tick_params( color=fg_color, labelcolor=fg_color)
+axes.xaxis.set_tick_params(color=fg_color, labelcolor=fg_color)
 
-axes.yaxis.set_tick_params( color=fg_color, labelcolor=fg_color)
+axes.yaxis.set_tick_params(color=fg_color, labelcolor=fg_color)
 
 axes.axhline(y=0, color='w')
 
@@ -65,14 +97,14 @@ for spine in axes.spines.values():
 
 #plot1=plt.plot(ee, y, c='y', linewidth=0.5,axes=axes)
 
-plot2=plt.scatter(ee,y,c='y', marker='.')
+#plot2=plt.scatter(ee,y,c='y',marker='.')
 
-plot3=plt.scatter(x['pred'],y, c='w',marker='.')
+#plot3=plt.scatter(X['pred'],y,c='b',marker='.')
 
  
 
 plot2=plt.plot(t, ee, c='y', linewidth=0.5,axes=axes)
 
-plot3=plt.plot(t, y, c='w', linewidth=0.5,axes=axes)
+plot3=plt.plot(t, y, c='b', linewidth=0.5,axes=axes)
 
-plot3=plt.plot(t, x['pred'], c='r', linewidth=0.5,axes=axes)
+plot4=plt.plot(t, X['pred'], c='r', linewidth=0.5,axes=axes)
